387498354a7c2faef8a1008bea9327e0ddd2a95a
iftekhar-hossen <iftekharhossen4555@gmail.com>
2024-10-08 15:47:56 +0600

    Refactor environment variables and update CORS headers

---
+++ middleware.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+export function middleware(req) {
+  const res = NextResponse.next();
+  // Set CORS headers
+  res.headers.set('Access-Control-Allow-Origin', '*'); // Adjust according to your needs
+  res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+  return res;
+}
